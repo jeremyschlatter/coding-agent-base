65090ff30df4611ce771c85618d7fca466e97991
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2025-10-24 15:18:35 -0700

    save installer in repo

--- scripts/claude_code_web_init.sh
+++ scripts/claude_code_web_init.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -e
+
 # Only run in remote environments
 if [ "$CLAUDE_CODE_REMOTE" != "true" ]; then
   exit 0
 fi
 
-curl -fsSL https://raw.githubusercontent.com/jeremyschlatter/nix-direnv/main/install.sh | sh
+"$CLAUDE_PROJECT_DIR"/scripts/nix-installer-x86_64-linux install --determinate --no-confirm
